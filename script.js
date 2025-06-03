c514f8b4512237da7c76c572bda100ffa820fb5e
Colssbcd <colsssbcd@gmail.com>
2025-06-03 14:11:29 +0530

    .
    .

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Adding an alert when the button is clicked
+const learnMoreButton = document.querySelector('#hero button');
+
+learnMoreButton.addEventListener('click', function() {
+    alert('You clicked the Learn More button!');
+});
+
+// You can add more JavaScript code here to handle user interactions,
+// manipulate the DOM, fetch data, etc.
